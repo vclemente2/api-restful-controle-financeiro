269022afe43104578656caecbbff1120bdbb76f5
Vinicius Bastos Clemente <vclemente2@hotmail.com>
2023-02-22 18:50:54 -0300

    Implementação do Endpoint de login do usuário.

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,35 +1,49 @@
-const { Router } = require('express');
+const { Router } = require('express')
 
-const { cadastrarUsuario, logarUsuario, detalharUsuario, atualizarUsuario } = require('../controladores/usuarios');
+const { cadastrarUsuario, logarUsuario, detalharUsuario, atualizarUsuario } = require('../controladores/usuarios')
 const { listarCategoria } = require('../controladores/categoria');
-const { listarTransacao, detalharTransacao, cadastrarTransacao, atualizarTransacao, excluirTransacao, obterExtrato } = require('../controladores/transacao');
-const { verificarNomeEmailSenha, verificarEmailExistente } = require('../intermediarios/usuarios');
+const { listarTransacao, detalharTransacao, cadastrarTransacao, atualizarTransacao, excluirTransacao, obterExtrato } = require('../controladores/transacao')
+const { verificarEmailExistente, verificarEmail, verificarNome, verificarSenha, verificarSenhaValida, gerarToken } = require('../intermediarios/usuarios')
 
-const rotas = Router();
+const rotas = Router()
 
-rotas.post('/usuario', verificarNomeEmailSenha , verificarEmailExistente, cadastrarUsuario,);
+rotas.post(
+    '/usuario',
+    verificarNome,
+    verificarEmail,
+    verificarSenha,
+    verificarEmailExistente,
+    cadastrarUsuario
+)
 
-rotas.post('/login', logarUsuario);
+rotas.post(
+    '/login',
+    verificarEmail,
+    verificarSenha,
+    verificarEmailExistente,
+    verificarSenhaValida,
+    logarUsuario
+)
 
-rotas.use(() => { }); //Passar o intermediário de autenticação como intermediário
+rotas.use(() => { }) //Passar o intermediário de autenticação como intermediário
 
-rotas.get('/usuario', detalharUsuario);
+rotas.get('/usuario', detalharUsuario)
 
-rotas.put('/usuario', atualizarUsuario);
+rotas.put('/usuario', atualizarUsuario)
 
-rotas.get('/categoria', listarCategoria);
+rotas.get('/categoria', listarCategoria)
 
-rotas.get('/transacao', listarTransacao);
+rotas.get('/transacao', listarTransacao)
 
-rotas.get('/transacao/:id', detalharTransacao);
+rotas.get('/transacao/:id', detalharTransacao)
 
-rotas.post('/transacao', cadastrarTransacao);
+rotas.post('/transacao', cadastrarTransacao)
 
-rotas.put('/transacao/:id', atualizarTransacao);
+rotas.put('/transacao/:id', atualizarTransacao)
 
-rotas.delete('/transacao/:id', excluirTransacao);
+rotas.delete('/transacao/:id', excluirTransacao)
 
-rotas.get('/transacao/extrato', obterExtrato);
+rotas.get('/transacao/extrato', obterExtrato)
 
 
-module.exports = rotas;
+module.exports = rotas
--- src/utils/utilsUsuarios.js
+++ src/utils/utilsUsuarios.js
@@ -3,10 +3,12 @@ const pool = require('../conexao/conexao')
 const buscarUsuarioPeloEmail = async (email) => {
     const query = `SELECT * FROM usuarios WHERE email = $1`
 
-    const usuarioEncontrado = await pool.query(query, [email])
+    const usuarioEncontrado = await pool.query(query, [email.trim()])
 
     return usuarioEncontrado
 }
+
+
 module.exports = {
     buscarUsuarioPeloEmail
 }
