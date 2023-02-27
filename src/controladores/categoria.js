3533bd85cfecce6dfec61aed7883095b895e3113
WilliamDiangellis <diangellis2014@gmail.com>
2023-02-27 14:28:06 -0300

    Implementação do endpoint listar categoria

--- src/controladores/categoria.js
+++ src/controladores/categoria.js
@@ -1,6 +1,16 @@
-const listarCategoria = (req, res) => {
+const pool = require('../conexao/conexao')
 
-};
+const listarCategoria = async (req, res) => {
+
+	try {
+		const { rows } = await pool.query('SELECT * FROM categorias')
+
+        return res.status(200).json(rows)
+	} catch (error) {
+		return res.status(500).json('Erro interno do servidor')
+	}
+
+}
 
 module.exports = {
     listarCategoria
