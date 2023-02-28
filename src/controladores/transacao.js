442aa7151bfebfca0e587a8761e9a79bb44ce3f2
WilliamDiangellis <diangellis2014@gmail.com>
2023-02-27 22:05:48 -0300

    Implementação do endpoint de listar transação do usuario

--- src/controladores/transacao.js
+++ src/controladores/transacao.js
@@ -1,11 +1,23 @@
 const pool = require('../conexao/conexao')
 
-const listarTransacao = (req, res) => {
-
+const listarTransacao = async (req, res) => {
+    const { id } = req.usuario
+    try {
+        const query = `
+        SELECT t.id, t.tipo, t.descricao, t.valor, t.data, t.usuario_id, t.categoria_id, c.descricao as categoria_nome
+        FROM transacoes t 
+        JOIN categorias c ON t.categoria_id = c.id
+        WHERE t.usuario_id = $1;
+        `
+        const { rows } = await pool.query(query, [id])
+        return res.status(200).json(rows)
+    } catch (error) {
+        return res.status(500).json({mensagem: error.message})
+    } 
 }
 
 const detalharTransacao = (req, res) => {
-
+    
 }
 
 const cadastrarTransacao = async (req, res) => {
